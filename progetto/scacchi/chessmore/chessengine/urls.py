b41c453f8795f742560d00e99c462ac3e2c302e1
Edward Pini <edwp1101@gmail.com>
2023-05-27 16:31:59 +0200

    rimosso tutte le cartelle non più utilizzate

--- progetto/scacchi/chessmore/chessengine/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = "chessengine"
-
-urlpatterns = [
-    path("<str:variant>/<str:room_number>/<str:name>/", chess_game, name="chess_game")
-]
--- progetto/scacchi/chessmore/chessmore/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .consumers import WSConsumerChess
-
-ws_urlpatterns = [
-    path("ws/chessws/<str:room_name>/<str:variant>/", WSConsumerChess.as_asgi()),
-]
